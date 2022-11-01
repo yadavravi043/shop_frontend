e065588f1945e12f7fd75831adeefbc600f567b6
yadavravi043 <ravi18016925@gmail.com>
2022-11-01 18:51:25 +0530

    redux is implemented

--- src/index.js
+++ src/index.js
@@ -4,9 +4,15 @@ import './bootstrap.min.css';
 //import './index.css';
 import App from './App';
 
+
+//import { Provider } from 'react';
+//import store from './store'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+   // <Provider store={store}>
+        <App />
+   // </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
