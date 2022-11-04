144d4623b0a7b639b5d75417e44cd5d62241bed8
yadavravi043 <ravi18016925@gmail.com>
2022-11-04 22:09:36 +0530

    pta nai

--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ import './bootstrap.min.css';
 import App from './App';
 
 
-//import { Provider } from 'react';
-//import store from './store'
+import { Provider } from 'react-redux';
+import store from './store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-   // <Provider store={store}>
+    <Provider store={store}>
         <App />
-   // </Provider>
+    </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/store.js
+++ src/store.js
@@ -1,13 +1,16 @@
 import {createStore,combineReducers,applyMiddleware} from 'redux'
+import { configureStore } from '@reduxjs/toolkit'
 import thunk from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 const reducer=combineReducers({})
 const initialState={}
 const middleWare=[thunk]
 
-const store=createStore(
+//const store=createStore(
+const store=configureStore(
     reducer,
     initialState,
     composeWithDevTools(applyMiddleware(...middleWare))
 )
-module.exports=store
+//module.exports=store
+export default store
