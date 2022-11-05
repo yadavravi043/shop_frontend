556fd1338556b4652bb80693b1dc4f38f633a47a
yadavravi043 <ravi18016925@gmail.com>
2022-11-05 21:48:39 +0530

    dont know

--- src/index.js
+++ src/index.js
@@ -5,16 +5,13 @@ import './bootstrap.min.css';
 import App from './App';
 
 
-import { Provider } from 'react-redux';
-import store from './store'
+//import { Provider } from 'react-redux';
+//import store from './store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Provider store={store}>
+    //<Provider store={store}>
         <App />
-    </Provider>
+    //</Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
