4ffcfa102ae761f453a9ac1a5c1c39df9d7d1320
yadavravi043 <ravi18016925@gmail.com>
2022-11-14 18:55:56 +0530

    bringing redux state into homescreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,46 +1,80 @@
-import React,{useState,useEffect} from 'react'
-import {Row,Col} from 'react-bootstrap'
-import Product from '../components/Product'
-//import products from '../products'
-import axios from 'axios'
+// import React,{useState,useEffect} from 'react'
+// import {Row,Col} from 'react-bootstrap'
+// import Product from '../components/Product'
+// //import products from '../products'
+// import axios from 'axios'
+// const HomeScreen = () => {
+//   const [products,setProducts]=useState([])
+//   // useEffect(()=>{
+//   //   const fetchProdcts = async ()=>{
+//   //   const { data } = await axios.get('/api/products')
+//   //   setProducts(data)
+//   //   }
+//   //   fetchProdcts()
+//   // },[])
+
+//   //here we access products from backend so mention proxy in frontend package.json
+//   const fetchProdcts = async ()=>{
+//   const { data } = await axios.get('/api/products')
+//   setProducts(data)
+//   }
+//   useEffect(()=>{
+//     fetchProdcts()
+//   },[])
+//   return (
+//   <>
+//     <h1>Latest Products</h1>
+//     <Row>
+//     {
+//         products.map(it=> (
+//          <Col key={it._id} sm={12} md={6} lg={4} xl={3}>
+//           <Product product={it}/>
+//          </Col>
+//      ))
+//     }
+//     </Row>
+//   </>
+//   )
+// }
+// export default HomeScreen;
+
+
+
+
+
+
+
+
+//after action and redux so didnt require localstate ie usestate
+import { useDispatch, useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { Row, Col } from "react-bootstrap";
+import Product from "../components/Product";
+import { listProducts } from "../actions/productActions";
 const HomeScreen = () => {
-  const [products,setProducts]=useState([])
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(listProducts());
+  }, [dispatch]);
 
-
-  // useEffect(()=>{
-  //   const fetchProdcts = async ()=>{
-  //   const { data } = await axios.get('/api/products')
-  //   setProducts(data)
-  //   }
-  //   fetchProdcts()
-  // },[])
-
-
-
-  //here we access products from backend so mention proxy in frontend package.json
-  const fetchProdcts = async ()=>{
-  const { data } = await axios.get('/api/products')
-  setProducts(data)
-  }
-  useEffect(()=>{
-    fetchProdcts()
-  },[])
-
-
+  //select product here from state so selectors comesin
+  //take productList which is mention in store.js
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
+  //const products=[]
   return (
-  <>
-    <h1>Latest Products</h1>
-    <Row>
-    {
-        products.map(it=> (
-         <Col key={it._id} sm={12} md={6} lg={4} xl={3}>
-          <Product product={it}/>
-         </Col>
-     ))
-    }
-    </Row>
-  </>
-  )
-}
+    <>
+      <h1>Latest Products</h1>
+      {loading?(<h2>loading...</h2>):error?(<h3>{error}</h3>): <Row>
+      {products.map((it) => (
+        <Col key={it._id} sm={12} md={6} lg={4} xl={3}>
+          <Product product={it} />
+        </Col>
+      ))}
+    </Row>}
+     
+    </>
+  );
+};
 
 export default HomeScreen;
