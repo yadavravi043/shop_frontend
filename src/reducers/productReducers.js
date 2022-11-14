b7538ed167be4b32b4eb1f29de16a1be492e0a96
yadavravi043 <ravi18016925@gmail.com>
2022-11-14 16:09:53 +0530

    reducer with constant

---
+++ src/reducers/productReducers.js
@@ -0,0 +1,17 @@
+//use in store so import there
+import { PRODUCT_LIST_REQUEST,
+    PRODUCT_LIST_SUCCESS,
+    PRODUCT_LIST_FAIL } from "../constants/productConstants"
+
+export const productListReducer = (state={products: [] }, action)=>{
+   switch (action. type){
+     case PRODUCT_LIST_REQUEST:
+       return { loading: true, products: []}
+     case PRODUCT_LIST_SUCCESS:
+       return {loading: false, products: action. payload }
+     case PRODUCT_LIST_FAIL:
+       return { loading: false, error: action.payload }
+     default:
+       return state
+       }
+    }
