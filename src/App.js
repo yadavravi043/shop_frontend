6188ed1a5e35b4361760c0948398c251438be5bf
yadavravi043 <ravi18016925@gmail.com>
2022-11-15 12:31:07 +0530

    cart added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen";
 function App() {
   return (
     <BrowserRouter>
@@ -15,6 +16,7 @@ function App() {
         <Routes>
         <Route path='/' element={<HomeScreen/>} exact/>
         <Route path='/product/:id' element={<ProductScreen/>} />
+        <Route path='/cart/:id?' element={<CartScreen/>} />
         </Routes>
         </Container>
       </main>
