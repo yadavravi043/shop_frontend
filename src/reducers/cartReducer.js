8deaf8b74920a81d4dabc4525d7c9132b4962027
yadavravi043 <ravi18016925@gmail.com>
2022-11-22 12:41:25 +0530

    payment screen

--- src/reducers/cartReducer.js
+++ src/reducers/cartReducer.js
@@ -1,4 +1,6 @@
-import { CART_ADD_ITEM,CART_REMOVE_ITEM, CART_SAVE_SHIPPING_ADDRESS } from "../constants/cartConstant"
+import { CART_ADD_ITEM,
+    CART_REMOVE_ITEM,
+     CART_SAVE_SHIPPING_ADDRESS ,CART_SAVE_PAYMENT_METHOD} from "../constants/cartConstant"
 export const cartReducer=(state={cartItems:[], shippingAddress:{}} ,action)=>{
     switch(action.type){
         case CART_ADD_ITEM:
@@ -25,6 +27,11 @@ export const cartReducer=(state={cartItems:[], shippingAddress:{}} ,action)=>{
             return{
                 ...state,
                 shippingAddress:action.payload,
+            } 
+        case CART_SAVE_PAYMENT_METHOD:
+            return{
+                ...state,
+                paymentMethod:action.payload,
             } 
         default:
             return state
--- src/screens/PaymentScreen.js
+++ src/screens/PaymentScreen.js
@@ -1,11 +1,72 @@
-import React from 'react'
-
+import React, { useState, useEffect } from "react";
+import { Link, Navigate, useNavigate } from "react-router-dom";
+import { Form, Button, Col } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import FormContainer from "../components/FormContainer";
+import { savePaymentMethod } from "../actions/cartActions";
+import CheckoutSteps from "../components/CheckoutSteps";
 const PaymentScreen = () => {
-  return (
-    <div>
-      payment
-    </div>
-  )
-}
+  const cart = useSelector((state) => state.cart);
+  const { shippingAddress } = cart;
 
-export default PaymentScreen
+  if (!shippingAddress) {
+    navigate("/shipping");
+  }
+
+  const [paymentMethod, setPaymentMethod] = useState("PayPal");
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(savePaymentMethod(paymentMethod));
+    navigate("/placeorder");
+  };
+  return (
+    <FormContainer>
+      <CheckoutSteps step1 step2 step3 />
+      <h1>Payment Method</h1>
+      <Form onSubmit={submitHandler}>
+        <Form.Group>
+          <Form.Label as="legend">Select Method</Form.Label>
+          <Col>
+          <Form.Check
+          type='radio'
+            label='Paypal or Credit Card'
+            id='PayPal'
+            name='paymentMethod'
+            value='Paypal'
+            checked
+            onChange={(e)=>setPaymentMethod(e.target.value)}
+            ></Form.Check>
+
+            <Form.Check
+            type='radio'
+            label='Stripe'
+            id='Stripe'
+            name='paymentMethod'
+            value='Stripe'
+            onChange={(e)=>setPaymentMethod(e.target.value)}
+            ></Form.Check>
+
+            <Form.Check
+            type='radio'
+            label='Razor Pay'
+            id='Razor Pay'
+            name='paymentMethod'
+            value='Razor Pay'
+            onChange={(e)=>setPaymentMethod(e.target.value)}
+            ></Form.Check>
+            
+            </Col>
+            </Form.Group>
+
+        <Button type="submit" variant="primary">
+          continue
+        </Button>
+      </Form>
+    </FormContainer>
+  );
+};
+
+export default PaymentScreen;
